163480b4f232aa571b7567a97f9c3cfde5c4c7f1
Miguel  de la Mata <sebidelamata@gmail.com>
2023-09-25 13:23:27 -0600

    ship placement edge cases

--- __tests__/gameboard.test.js
+++ __tests__/gameboard.test.js
@@ -65,4 +65,20 @@ describe("Gameboard", () => {
             [0,0,0,0,0,0,0,0,0,0]
         ]);
     });
+    test('does not allow ship placed off board direction down', () => {
+        expect(testBoard.placeShip(5,1,1,"down")).toEqual('Can not place ship off of the board!');
+    });
+    test('does not allow ship placed off board direction up', () => {
+        expect(testBoard.placeShip(5,9,1,"up")).toEqual('Can not place ship off of the board!');
+    });
+    test('does not allow ship placed off board direction left', () => {
+        expect(testBoard.placeShip(5,1,9,"left")).toEqual('Can not place ship off of the board!');
+    });
+    test('does not allow ship placed off board direction right', () => {
+        expect(testBoard.placeShip(5,1,1,"right")).toEqual('Can not place ship off of the board!');
+    });
+    test('can not place ship on top of another ship', () => {
+        testBoard.placeShip(2,1,1,"down");
+        expect(testBoard.placeShip(2,2,1,"down")).toEqual('Can not place ship on top of another ship');
+    });
 });
--- src/gameboard.js
+++ src/gameboard.js
@@ -15,20 +15,60 @@ module.exports = class gameboard{
         let newShip = new ship(shipLength);
         if(noseDirection == 'left'){
             for(let i=0; i<shipLength; i++){
+                if((noseColumn + i) > 9){
+                    return 'Can not place ship off of the board!';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
+                if(this.board[noseRow][noseColumn + i] == 1){
+                    return 'Can not place ship on top of another ship';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
                 this.board[noseRow][noseColumn + i] = 1;
             }
         }
         if(noseDirection == 'right'){
+            for(let i=0; i<shipLength; i++){
+                if((noseColumn - i) < 0){
+                    return 'Can not place ship off of the board!';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
+                if(this.board[noseRow][noseColumn - i] == 1){
+                    return 'Can not place ship on top of another ship';
+                }
+            }
             for(let i=0; i<shipLength; i++){
                 this.board[noseRow][noseColumn - i] = 1;
             }
         }
         if(noseDirection == 'up'){
             for(let i=0; i<shipLength; i++){
+                if((noseRow + i) > 9){
+                    return 'Can not place ship off of the board!';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
+                if(this.board[noseRow + i][noseColumn] == 1){
+                    return 'Can not place ship on top of another ship';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
                 this.board[noseRow + i][noseColumn] = 1;
             }
         }
         if(noseDirection == 'down'){
+            for(let i=0; i<shipLength; i++){
+                if((noseRow - i) < 0){
+                    return 'Can not place ship off of the board!';
+                }
+            }
+            for(let i=0; i<shipLength; i++){
+                if(this.board[noseRow - i][noseColumn] == 1){
+                    return 'Can not place ship on top of another ship';
+                }
+            }
             for(let i=0; i<shipLength; i++){
                 this.board[noseRow - i][noseColumn] = 1;
             }
